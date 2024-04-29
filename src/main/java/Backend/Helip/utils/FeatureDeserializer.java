f365f27437d2c658687adeb371c0953622a06f29
kridesav <savolainenkristjan@gmail.com>
2024-04-30 00:31:40 +0300

    test v19

--- src/main/java/Backend/Helip/utils/FeatureDeserializer.java
+++ src/main/java/Backend/Helip/utils/FeatureDeserializer.java
@@ -5,19 +5,26 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import Backend.Helip.models.Feature;
 
 import java.io.IOException;
 
 public class FeatureDeserializer extends JsonDeserializer<Feature> {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public FeatureDeserializer() {
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Feature.class, this);
+        mapper.registerModule(module);
+    }
+
+    @Override
     public Feature deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        ObjectMapper mapper = (ObjectMapper) p.getCodec();
         JsonNode node = p.getCodec().readTree(p);
         Feature feature = mapper.treeToValue(node, Feature.class);
-    
         feature.updateLocationFromGeometry();
-    
         return feature;
     }
 }
