d1722a6b43aab3627d16adfe979414e7479e0a12
kridesav <savolainenkristjan@gmail.com>
2024-04-29 22:19:40 +0300

    Feature -> features

--- src/main/java/Backend/Helip/repositories/FeatureRepository.java
+++ src/main/java/Backend/Helip/repositories/FeatureRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface FeatureRepository extends JpaRepository<Feature, Long> {
-    @Query(value = "SELECT * FROM Feature f WHERE ST_DWithin(CAST(f.location AS geography), CAST(:location AS geography), :radius)", nativeQuery = true)
+    @Query(value = "SELECT * FROM features f WHERE ST_DWithin(CAST(f.location AS geography), CAST(:location AS geography), :radius)", nativeQuery = true)
     List<Feature> findWithinRadius(@Param("location") String location, @Param("radius") double radius);
 }
