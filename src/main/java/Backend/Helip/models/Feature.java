687f463d2538487c72bb93217810e5887c6ffd6c
kridesav <savolainenkristjan@gmail.com>
2024-04-30 00:25:44 +0300

    test v17

--- src/main/java/Backend/Helip/models/Feature.java
+++ src/main/java/Backend/Helip/models/Feature.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
@@ -14,10 +16,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import Backend.Helip.utils.FeatureDeserializer;
 
+
+
 @Entity
 @Table(name = "features")
 @JsonDeserialize(using = FeatureDeserializer.class)
 public class Feature {
+    private static final GeometryFactory geometryFactory = new GeometryFactory();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "feature_id")
@@ -77,5 +82,15 @@ public class Feature {
 
     public void setGeometry(Geometry geometry) {
         this.geometry = geometry;
+    }
+
+    public void updateLocationFromGeometry() {
+        if (this.geometry != null && this.geometry.getCoordinates() != null) {
+            Double[] coordinates = this.geometry.getCoordinates();
+            if (coordinates.length > 1) {
+                Point location = geometryFactory.createPoint(new Coordinate(coordinates[0], coordinates[1]));
+                this.setLocation(location);
+            }
+        }
     }
 }
--- src/main/java/Backend/Helip/utils/FeatureDeserializer.java
+++ src/main/java/Backend/Helip/utils/FeatureDeserializer.java
@@ -8,29 +8,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import Backend.Helip.models.Feature;
 
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Point;
-
 import java.io.IOException;
 
 public class FeatureDeserializer extends JsonDeserializer<Feature> {
-    private static final GeometryFactory geometryFactory = new GeometryFactory();
-
-    @Override
     public Feature deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         ObjectMapper mapper = (ObjectMapper) p.getCodec();
         JsonNode node = p.getCodec().readTree(p);
         Feature feature = mapper.treeToValue(node, Feature.class);
-
-        if (feature.getGeometry() != null && feature.getGeometry().getCoordinates() != null) {
-            Double[] coordinates = feature.getGeometry().getCoordinates();
-            if (coordinates.length > 1) {
-                Point location = geometryFactory.createPoint(new Coordinate(coordinates[0], coordinates[1]));
-                feature.setLocation(location);
-            }
-        }
-
+    
+        feature.updateLocationFromGeometry();
+    
         return feature;
     }
 }
