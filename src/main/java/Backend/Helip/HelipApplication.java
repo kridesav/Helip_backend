1fe9c402e642bbdb70e167128b4048961ed40715
kridesav <savolainenkristjan@gmail.com>
2024-04-29 16:31:37 +0300

    import new .json

--- src/main/java/Backend/Helip/HelipApplication.java
+++ src/main/java/Backend/Helip/HelipApplication.java
@@ -12,7 +12,7 @@ public class HelipApplication {
         ConfigurableApplicationContext context = SpringApplication.run(HelipApplication.class, args);
         FeatureService featureService = context.getBean(FeatureService.class);
         try {
-            featureService.importJson("lipas_kaikki_pisteet.json");
+            featureService.importJson("lipas_kaikki_pisteetC2.json");
         } catch (Exception e) {
             e.printStackTrace();
         }
